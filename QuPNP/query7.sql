576c2c4ea7c19f4ef08d95314fac4563f4537b19
Analista UNITIC <analista.unitic@outlook.com>
2025-08-07 17:17:07 -0500

    poblar MAESTRA SISPOL

--- QuPNP/query7.sql
+++ QuPNP/query7.sql
@@ -2,7 +2,6 @@ set rowcount 0
 -- select top 20 *from dbo.OPERATIVIDAD_VEHICULO
 -- delete from dbo.OPERATIVIDAD_VEHICULO
 
-
 exec sys.sp_spaceused OPERATIVIDAD_VEHICULO
 exec transporte.sys.sp_spaceused OPERATIVIDAD_VEHICULO
 return
@@ -21,9 +20,47 @@ nullif(coalesce(nullif(Id_TipoEstadoRegistro, 1), nullif(Id_TipoEstadoRegistro2,
 IdTipoMotivoInoperatividad, Id_TipoEstadoVehiculo, Id_TipoMaestranza, Id_TipoDocumento,
 Nro_Documento, Fec_Documento, Fec_Operatividad, Observacion, UsuarioI, FechaI, Estado
 from(select lead(Id_TipoEstadoRegistro)over(partition by t.id_vehiculo order by t.fechaI desc) Id_TipoEstadoRegistro2,t.*
-from(select row_number()over(partition by id_vehiculo order by fechaI desc)item, *
+from(select row_number()over(partition by id_vehiculo order by fechaI desc)item,
+sum(1)over(partition by id_vehiculo)cta,*
 from transporte.dbo.OPERATIVIDAD_VEHICULO)t cross apply (values(1),(2))tt(item)
 where t.item = tt.item)t
-where not t.Id_TipoEstadoRegistro2 is null
+where t.cta = 1 or (not t.Id_TipoEstadoRegistro2 is null)
 
 set identity_insert dbo.OPERATIVIDAD_VEHICULO off
+
+
+
+-- NOTA: PARA CONFIRMAR EL TAMAÑO DE LA DATA POBLADA
+-- =================================================
+
+use sispap1;
+exec sp_spaceused OPERATIVIDAD_VEHICULO
+
+select count(1)
+from(select row_number()over(partition by id_vehiculo order by(select 1))item
+from dbo.operatividad_vehiculo)t where item = 1
+
+select count(1)
+from(select row_number()over(partition by id_vehiculo order by(select 1))item
+from transporte.dbo.operatividad_vehiculo)t where item = 1
+
+
+
+
+-- select*from dbo.TIPO_VEHICULO
+-- select*from transporte.dbo.TIPO_VEHICULO
+
+-- select*from dbo.TIPO_MARCA
+-- select*from transporte.dbo.TIPO_MARCA
+
+-- select*from dbo.TIPO_MODELO
+-- select*from transporte.dbo.TIPO_MODELO
+
+-- select*from dbo.TIPO_COLOR
+-- select*from transporte.dbo.TIPO_COLOR
+
+-- select*from dbo.TIPO_TRANSMISION
+-- select*from transporte.dbo.TIPO_TRANSMISION
+
+-- select*from dbo.TIPO_COMBUSTIBLE
+-- select*from transporte.dbo.TIPO_COMBUSTIBLE
--- QuPNP/query8.sql
+++ QuPNP/query8.sql
@@ -1,3 +1,22 @@
--- use transporte;
+set rowcount 100
+-- exec sp_spaceused PROG_VEHICULO
+-- select*from mastertable('dbo.MasterPNP')
 
-select*from dbo.operatividad_vehiculo;
+
+select*from dbo.MasterPNP
+
+
+return
+-- insert into dbo.MasterPNP
+select
+str(CAST(RAND(CHECKSUM(NEWID())) * 900000000 + 100000000 AS BIGINT), 9, 0),
+str(t.ma76, 8, 0), str(t.ma98, 8, 0),
+left(ltrim(t.APEPAT), 90), left(ltrim(t.APEMAT), 90),
+left(ltrim(t.NOMBRE), 90), t.ma02, left(ltrim(t.DescripcionC),90),
+tt.id_unidad, tt.UltimaUnidad, 1, 'ACTIVIDAD', getdate(), getdate()
+from(select row_number()over(order by (select 1))item, ma76, ma98, APEPAT, APEMAT, NOMBRE, ma02, tt.DescripcionC
+from transporte.dbo.MASPOL t cross apply transporte.dbo.Tipo_Grado tt
+where t.ma02 = tt.Id_TipoGrado)t cross apply
+(select row_number()over(order by (select 1))item, id_unidad, UltimaUnidad
+from dbo.unidad)tt
+where t.item = tt.item
