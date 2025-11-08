cfc567b981d4138c374adddf2dc089b760d6c8f3
Oliver Suarez <oliver.suareztinoco@gmail.com>
2025-11-08 07:10:06 -0500

    feat:progresando en cambios
    Detalle del progreso ...

--- QuPNP/query38.sql
+++ QuPNP/query38.sql
@@ -3,7 +3,6 @@
 -- delete dbo.prog_ruta where Id_ProgRuta > 39
 -- delete dbo.prog_extraord where Id_ProgExtraOrd > 3
 
-
 select*from dbo.prog_extraord
 select*from dbo.prog_ruta
 select*from dbo.prog_eo_grifo
