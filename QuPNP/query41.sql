eddd235e2743f69df04f6fa80826422acef9eafb
oliver suarez tinoco <oliver.suareztinoco@gmail.com>
2025-11-13 19:12:39 -0500

    feat:cambios propuestos
    nuevos cambios para el detalle

--- QuPNP/query41.sql
+++ QuPNP/query41.sql
@@ -1,11 +1,18 @@
--- select*from mastertable('dbo.prog_eo_grifo')
 -- declare @Utabla22 tabla_generico
 -- insert into @Utabla22
 -- exec dbo.usp_listar_tablas 'dbo.prog_eo_grifo'
 -- select*from @Utabla22
 
+-- select*from dbo.menuTransportes
+
 -- delete dbo.prog_ruta where Id_ProgRuta > 39
 -- delete dbo.prog_extraord where Id_ProgExtraOrd > 3
+
+-- select*from mastertable('dbo.prog_extraord')
+-- select*from mastertable('dbo.prog_ruta')
+-- select*from mastertable('dbo.prog_eo_grifo')
+
+select*from dbo.menu
 
 
 select*from dbo.prog_extraord
