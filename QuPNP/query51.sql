c1f6f4febebcec7d51c6d63c0a7b2f001a815471
oliver suarez tinoco <oliver.suareztinoco@gmail.com>
2025-11-28 20:15:22 -0500

    cambios en sp
    Detalle de los cambios

--- QuPNP/query51.sql
+++ QuPNP/query51.sql
@@ -2,12 +2,27 @@
 -- select 8, 'dbo.prog_abastecimiento_diario'
 set rowcount 50
 
--- Id_ProgVehiculo Anio Mes
-select*from dbo.prog_vehiculo
+;with tmp001_sep(t,r,i,a)as(
+    select*from(values('|','~','^','*'))t(sepCampo,sepReg,sepLst,sepAux)
+)
+select concat(t, t, t, t, t, anno, t, mes)
+from(select max(anio) anno, max(mes) mes from dbo.prog_vehiculo group by anio, mes)t,
+tmp001_sep
 
 
+-- declare @Utabla tabla_generico
+-- insert into @Utabla
+-- exec dbo.usp_listar_tablas 'dbo.prog_abastecimiento_diario'
+-- select*from @Utabla
 
 select*from mastertable('dbo.prog_abastecimiento_diario')
+
+-- declare @Utabla tabla_generico
+-- insert into @Utabla
+-- exec dbo.usp_listar_tablas 'dbo.prog_tarjeta_multiflota'
+-- select*from @Utabla
+
+-- select*from mastertable('dbo.prog_tarjeta_multiflota')
 
 return
 
